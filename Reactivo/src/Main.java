2f9531297011b37fa08f65a942c63fd747664a26
juadango <juadango@bancolombia.com.co>
2024-03-14 08:32:10 -0500

    Se agrega la suma
    Se agregalasuma

--- Reactivo/src/Main.java
+++ Reactivo/src/Main.java
@@ -4,16 +4,15 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<Integer> numeros = List.of(18, 6, 4, 15, 55, 78, 12, 9, 8);
+        List<Integer> numerosFibo = new ArrayList<>();
         metodoInteractivo(numeros);
         metodoFuncional(numeros);
         Integer variableFibo = 10;
         for (int i = 0; i < variableFibo; i++) {
-            Integer resultFibo = fibonacci(i);
-            if (resultFibo % 2 == 0) {
-                System.out.println(resultFibo);
-            }
+            numerosFibo.add(fibonacci(i));
         }
-
+        System.out.println("La suma de los números pares en fibonacci es " + numerosFibo.stream().filter(d-> d% 2 ==0).
+                reduce((a,b) -> a+b).get());
     }
 
     public static void metodoInteractivo(List<Integer> numeros) {
