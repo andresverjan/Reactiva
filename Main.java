f58c1db47ae49a739c756d1848201c4b6286a9ea
Isabel Restrepo <mariaarpo8@gmail.com>
2024-03-14 07:39:47 -0500

    Ejercicio imperativo

--- Main.java
+++ Main.java
@@ -2,16 +2,24 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer>numeros = List.of(18,6,4,15,55,78,12,9,8);
-        int contador=0;
+        List<Integer> numeros = List.of(18, 6, 4, 15, 55, 78, 12, 9, 8);
+        int contador = 0;
 
-        for(int num: numeros){
-            if(num>10){
+        for (int num : numeros) {
+            if (num > 10) {
                 contador++;
                 System.out.println(num);
             }
         }
         System.out.println("\n");
-        System.out.println("Los numeros mayores a 10 son en tota: "+contador);
+        System.out.println("Los numeros mayores a 10 son en tota: " + contador);
+
+
+        long count = numeros.stream()
+                .filter(num -> num > 10)
+                .count();
+        System.out.println(count);
     }
+
+
 }
