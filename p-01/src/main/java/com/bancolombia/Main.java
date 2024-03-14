9e9446565834d7754fb28fc1e3295b5e52d3ad3d
Juan Felipe Giraldo <juangira@bancolombia.com.co>
2024-03-14 07:40:19 -0500

    Primero ejercicio

--- p-01/src/main/java/com/bancolombia/Main.java
+++ p-01/src/main/java/com/bancolombia/Main.java
@@ -1,7 +1,21 @@
 package com.bancolombia;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Integer> numeros = List.of(18, 6, 4, 15, 55, 78, 12, 9, 8);
+        int counter = 0;
+
+        for(int numero : numeros) {
+            if(numero > 10){
+                counter++;
+            }
+        }
+
+        System.out.println("Cantidad de números mayores a 10: " + counter);
+
+        int counter2 = (int) numeros.stream().filter(numero -> numero > 10).count();
+        System.out.println("Cantidad de números mayores a 10: " + counter2);
     }
 }
