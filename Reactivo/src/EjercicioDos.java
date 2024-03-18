f2562c0f39a7752b88296cc415da7e94f27890e2
juadango <juadango@bancolombia.com.co>
2024-03-18 09:10:41 -0500

    Mejora
    Correccion imports

--- Reactivo/src/EjercicioDos.java
+++ Reactivo/src/EjercicioDos.java
@@ -2,7 +2,6 @@ import model.Persona;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class EjercicioDos {
     public static void main(String[] args) {
@@ -18,17 +17,19 @@ public class EjercicioDos {
         Persona persona10 = new Persona("Elena", "García", "112233445", 33, "Libra");
         Persona persona11 = new Persona("Pablo", "Muñoz", "554433221", 38, "Piscis");
         Persona persona12 = new Persona("Rosa", "Jiménez", "998877665", 29, "Sagitario");
-        List listPersonas = List.of(persona1, persona2, persona3, persona4, persona5, persona6, persona7, persona8, persona9, persona10, persona11, persona12);
-        metodoDeclarativo(listPersonas, 25, "Acuario");
-        metodoImperativo(listPersonas, 25, "Acuario");
+        List<Persona> listPersonas = List.of(persona1, persona2, persona3, persona4, persona5, persona6, persona7, persona8, persona9, persona10, persona11, persona12);
+        int edadBuscar = 25;
+        String signoBuscar = "Acuario";
+        metodoDeclarativo(listPersonas, edadBuscar, signoBuscar);
+        metodoImperativo(listPersonas, edadBuscar, signoBuscar);
     }
 
     private static void metodoImperativo(List<Persona> listPersonas, int anios, String signo) {
-         List<Persona> personaSigno = listPersonas.stream().filter(o -> o.getSigno().equalsIgnoreCase(signo))
-                 .collect(Collectors.toList());
-        List<Persona> personaAnio = listPersonas.stream().filter(o -> o.getEdad() > anios).collect(Collectors.toList());
-        System.out.println("LAS PERSONAS DEL SIGNO" + signo + " SON : " + personaSigno.toString());
-        System.out.println("LAS PERSONAS MAYORES DE " + anios +"  SON : " + personaAnio.toString());
+        List<Persona> personaSigno = listPersonas.stream().filter(o -> o.getSigno().equalsIgnoreCase(signo))
+                .toList();
+        List<Persona> personaAnio = listPersonas.stream().filter(o -> o.getEdad() > anios).toList();
+        System.out.println("LAS PERSONAS DEL SIGNO" + signo + " SON : " + personaSigno);
+        System.out.println("LAS PERSONAS MAYORES DE " + anios + "  SON : " + personaAnio);
     }
 
     private static void metodoDeclarativo(List<Persona> listPersonas, int anios, String signo) {
@@ -42,7 +43,7 @@ public class EjercicioDos {
                 listAnios.add(persona);
             }
         }
-        System.out.println("LAS PERSONAS DEL SIGNO" + signo + " SON : " + listSigno.toString());
-        System.out.println("LAS PERSONAS MAYORES DE " + anios +"  SON : " + listAnios.toString());
+        System.out.println("LAS PERSONAS DEL SIGNO" + signo + " SON : " + listSigno);
+        System.out.println("LAS PERSONAS MAYORES DE " + anios + "  SON : " + listAnios);
     }
 }
