4d387840c7f09f265d2490d754d98330a6a88c29
ssajm644 <sergio.jimenezm@gmail.com>
2024-03-18 21:56:15 -0500

    adicion funciones

--- src/Tarea1/EjercicioProgramacionFuncional.java
+++ src/Tarea1/EjercicioProgramacionFuncional.java
@@ -38,66 +38,83 @@ public class EjercicioProgramacionFuncional {
 
     public static void main(String[] args) {
 
-        Estudiante estudiante1 = new  Estudiante("Juan", "Pérez", "123456789", 30, "Aries");
-        Estudiante estudiante2 = new  Estudiante("María", "Gómez", "987654321", 25, "Virgo");
-        Estudiante estudiante3 = new  Estudiante("Carlos", "Martínez", "555444333", 40, "Capricornio");
-        Estudiante estudiante4 = new  Estudiante("Laura", "Rodríguez", "111222333", 35, "Tauro");
-        Estudiante estudiante5 = new  Estudiante("Pedro", "Sánchez", "999888777", 28, "Leo");
-        Estudiante estudiante6 = new  Estudiante("Ana", "Fernández", "666777888", 22, "Acuario");
-        Estudiante estudiante7 = new  Estudiante("David", "López", "333222111", 45, "Cáncer");
-        Estudiante estudiante8 = new  Estudiante("Sofía", "Díaz", "777666555", 32, "Géminis");
-        Estudiante estudiante9 = new  Estudiante("Javier", "Hernández", "888999000", 27, "Escorpio");
+        Estudiante estudiante1 = new Estudiante("Juan", "Pérez", "123456789", 30, "Aries");
+        Estudiante estudiante2 = new Estudiante("María", "Gómez", "987654321", 25, "Virgo");
+        Estudiante estudiante3 = new Estudiante("Carlos", "Martínez", "555444333", 40, "Capricornio");
+        Estudiante estudiante4 = new Estudiante("Laura", "Rodríguez", "111222333", 35, "Tauro");
+        Estudiante estudiante5 = new Estudiante("Pedro", "Sánchez", "999888777", 28, "Leo");
+        Estudiante estudiante6 = new Estudiante("Ana", "Fernández", "666777888", 22, "Acuario");
+        Estudiante estudiante7 = new Estudiante("David", "López", "333222111", 45, "Cáncer");
+        Estudiante estudiante8 = new Estudiante("Sofía", "Díaz", "777666555", 32, "Géminis");
+        Estudiante estudiante9 = new Estudiante("Javier", "Hernández", "888999000", 27, "Escorpio");
         Estudiante estudiante10 = new Estudiante("Elena", "García", "112233445", 33, "Libra");
         Estudiante estudiante11 = new Estudiante("Pablo", "Muñoz", "554433221", 38, "Piscis");
         Estudiante estudiante12 = new Estudiante("Rosa", "Jiménez", "998877665", 29, "Sagitario");
 
-       // System.out.println(estudiante11.getNombre().toString()); // prueba imprimir nombre est 11
+        // System.out.println(estudiante11.getNombre().toString()); // prueba imprimir nombre est 11
 
 
         List<Estudiante> estudiantes = List.of(
                 estudiante1, estudiante2, estudiante3, estudiante4, estudiante5, estudiante6,
                 estudiante7, estudiante8, estudiante9, estudiante10, estudiante11, estudiante12
         );
-
+        //llamando funciones para cada punto enviando lista de estudiantes
         //1. Usando el modo declarativo obtener todos los estudiantes que son de signo acuario.
+        estudiantesAcuario(estudiantes);
 
-        // Filtrar estudiantes de signo "Acuario"
-        List<Estudiante> estudiantesAcuario = estudiantes.stream()
-                .filter(estudiante -> estudiante.getSigno().equals("Acuario")).collect(Collectors.toList());
-
-       // Imprimir los estudiantes de signo acuario
-        System.out.println("1. Estudiantes que son de signo acuario modo declarativo: ");
-        estudiantesAcuario.forEach(estudiante -> System.out.println(estudiante.getNombre() + " " + estudiante.getApellido()));
-        System.out.println("**********************************: ");
         //2. Usando el modo declarativo obtener todos los estudiantes donde la edad es mayor a 25
+        estudiantesMayores25(estudiantes);
 
-        List<Estudiante> estudiantesMayores25 = estudiantes.stream()
-                .filter(estudiante -> estudiante.getEdad()>25).toList();
-
-        // Imprimir los estudiantes edad >25
-        System.out.println("2. Estudiantes que son mayores de 25 años modo declarativo: ");
-        estudiantesMayores25.forEach(estudiante -> System.out.println(estudiante.getNombre() + " " + estudiante.getApellido() + " " + estudiante.getEdad()));
-        System.out.println("**********************************: ");
 
         //3. Usando el modo imperativo obtener todos los estudiantes que son de signo acuario.
+        estudiantesAcuarioImperativo(estudiantes);
+        //4. Usando el modo imperativo obtener todos los estudiantes donde la edad es mayor a 25.
+        estudiantesMayores25Imperativo(estudiantes);
+
+    }
+        //Funciones
+
+    //1. Usando el modo declarativo obtener todos los estudiantes que son de signo acuario.
+    static void estudiantesAcuario(List<Estudiante> estudiantes) {
+        System.out.println("1. Estudiantes que son de signo acuario modo declarativo: ");
+        estudiantes.stream()
+                .filter(estudiante -> estudiante.getSigno().equals("Acuario"))
+                .forEach(estudiante -> System.out.println(estudiante.getNombre() + " " + estudiante.getApellido()));
+        System.out.println("**********************************: ");
+
+    }
+
+    //2. Usando el modo declarativo obtener todos los estudiantes donde la edad es mayor a 25
+    static void estudiantesMayores25(List<Estudiante> estudiantes) {
+        System.out.println("2. Estudiantes que son mayores de 25 años modo declarativo: ");
+        estudiantes.stream()
+                .filter(estudiante -> estudiante.getEdad() > 25)
+                .forEach(estudiante -> System.out.println(estudiante.getNombre() + " " + estudiante.getApellido() + " " + estudiante.getEdad()));
+        System.out.println("**********************************: ");
+    }
+
+
+    //3. Usando el modo imperativo obtener todos los estudiantes que son de signo acuario.
+    static void estudiantesAcuarioImperativo(List<Estudiante> estudiantes) {
+
         System.out.println("3. Estudiantes que son de signo acuario modo imperativo: ");
-        for (Estudiante estudiante : estudiantes) {
+         for (Estudiante estudiante : estudiantes) {
             if ("Acuario".equals(estudiante.getSigno())) {
-                estudiantesAcuario.add(estudiante);
-                System.out.println(estudiante.getNombre() + " " + estudiante.getApellido() );
+            System.out.println(estudiante.getNombre() + " " + estudiante.getApellido());
             }
         }
         System.out.println("**********************************: ");
+    }
 
-        //4. Usando el modo imperativo obtener todos los estudiantes donde la edad es mayor a 25.
+
+    //4. Usando el modo imperativo obtener todos los estudiantes donde la edad es mayor a 25.
+    static void estudiantesMayores25Imperativo(List<Estudiante> estudiantes) {
         System.out.println("4. Estudiantes que son mayores de 25 años modo imperativo: ");
-       // List<Estudiante> estudiantesMayoresDe25 = new ArrayList<>();
         for (Estudiante estudiante : estudiantes) {
-            if (estudiante.getEdad() > 25) {
-            //    estudiantesMayoresDe25.add(estudiante);
-                System.out.println(estudiante.getNombre() + " " + estudiante.getApellido() + " " + estudiante.getEdad());
-               }
+        if (estudiante.getEdad() > 25) {
+            System.out.println(estudiante.getNombre() + " " + estudiante.getApellido() + " " + estudiante.getEdad());
         }
-            System.out.println("**********************************: ");
-        }
-        }
+    }
+        System.out.println("**********************************: ");
+}
+}
