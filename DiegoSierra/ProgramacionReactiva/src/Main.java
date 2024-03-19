248a4005b0b5b2ba24ae3e9c7eb40aa928aa7161
dasierra <diegoalejo765@gmail.com>
2024-03-19 08:05:59 -0500

    tarea 1

--- DiegoSierra/ProgramacionReactiva/src/Main.java
+++ DiegoSierra/ProgramacionReactiva/src/Main.java
@@ -1,34 +1,35 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static List<Persona> obtenerEstudiantesAcuario(List<Persona> estudiantes) {
+    public static List<Persona> obtenerEstudiantesAcuario(List<Persona> estudiantes, String signo) {
         return estudiantes.stream()
-                .filter(estudiante -> estudiante.getSigno() == "Acuario" )
+                .filter(estudiante -> Objects.equals(estudiante.getSigno(), signo))
                 .collect(Collectors.toList());
     }
-    public static List<Persona> obtenerEstudiantesEdadMayorA25(List<Persona> estudiantes) {
+    public static List<Persona> obtenerEstudiantesEdadMayorA25(List<Persona> estudiantes, Integer edad) {
         return estudiantes.stream()
-                .filter(estudiante -> estudiante.getEdad() > 25)
+                .filter(estudiante -> estudiante.getEdad() > edad)
                 .collect(Collectors.toList());
     }
-    public static List<Persona> obtenerEstudiantesAcuarioImperativo(List<Persona> estudiantes) {
+    public static List<Persona> obtenerEstudiantesAcuarioImperativo(List<Persona> estudiantes, String signo) {
         List<Persona> estudiantesAcuario = new ArrayList<>();
         for (Persona estudiante : estudiantes) {
-            if ("Acuario".equalsIgnoreCase(estudiante.getSigno())) {
+            if (signo.equalsIgnoreCase(estudiante.getSigno())) {
                 estudiantesAcuario.add(estudiante);
             }
         }
         return estudiantesAcuario;
     }
-    public static List<Persona> obtenerEstudiantesEdadMayorA25Imperativo(List<Persona> estudiantes) {
+    public static List<Persona> obtenerEstudiantesEdadMayorA25Imperativo(List<Persona> estudiantes, Integer edad) {
         List<Persona> estudiantesEdadMayorA25 = new ArrayList<>();
         for (Persona estudiante : estudiantes) {
-            if (estudiante.getEdad() > 25) {
+            if (estudiante.getEdad() > edad) {
                 estudiantesEdadMayorA25.add(estudiante);
             }
         }
@@ -51,10 +52,10 @@ public class Main {
         Persona persona12 = new Persona("Rosa", "Jiménez", "998877665", 29, "Sagitario");
 
         List<Persona> personas = Arrays.asList(persona1, persona2, persona3,persona4,persona5,persona6,persona7,persona8,persona9,persona10,persona11,persona12);
-        List<Persona> estudiantesAcuario = obtenerEstudiantesAcuario(personas);
-        List<Persona> estudiantesMayor25 = obtenerEstudiantesEdadMayorA25(personas);
-        List<Persona> estudiantesAcuarioImperativo = obtenerEstudiantesAcuarioImperativo(personas);
-        List<Persona> estudiantesEdadMayorA25Imperativo = obtenerEstudiantesEdadMayorA25Imperativo(personas);
+        List<Persona> estudiantesAcuario = obtenerEstudiantesAcuario(personas, "Acuario");
+        List<Persona> estudiantesMayor25 = obtenerEstudiantesEdadMayorA25(personas, 25);
+        List<Persona> estudiantesAcuarioImperativo = obtenerEstudiantesAcuarioImperativo(personas,"Acuario");
+        List<Persona> estudiantesEdadMayorA25Imperativo = obtenerEstudiantesEdadMayorA25Imperativo(personas,25);
 
         System.out.println("Estudiantes del signo Acuario:");
         for (Persona estudiante : estudiantesAcuario) {
